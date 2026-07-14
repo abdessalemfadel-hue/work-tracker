7760460b048ec508769e3c33717ffe076cc00318
abdessalemfadel-hue <abdessalemfadel@gmail.com>
2026-07-14 15:44:18 +0400

    Use default WorkManager initialization

--- app/src/main/java/com/abdessalem/worktracker/WorkTrackerApp.kt
+++ app/src/main/java/com/abdessalem/worktracker/WorkTrackerApp.kt
@@ -1,20 +1,16 @@
 package com.abdessalem.worktracker
 
 import android.app.Application
-import androidx.work.Configuration
 import com.abdessalem.worktracker.worker.WorkScheduler
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @HiltAndroidApp
-class WorkTrackerApp : Application(), Configuration.Provider {
+class WorkTrackerApp : Application() {
     @Inject lateinit var workScheduler: WorkScheduler
 
     override fun onCreate() {
         super.onCreate()
         workScheduler.scheduleDailyChecks()
     }
-
-    override val workManagerConfiguration: Configuration
-        get() = Configuration.Builder().setMinimumLoggingLevel(android.util.Log.INFO).build()
 }
